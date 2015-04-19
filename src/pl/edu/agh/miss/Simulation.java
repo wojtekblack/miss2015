7e35f16b269c8c751f49b70791c9ef4da6e33298
Iwan Bugajski <iwan.bugajski@gmail.com>
2015-04-19 23:27:24 +0200

    Example results

--- src/pl/edu/agh/miss/Simulation.java
+++ src/pl/edu/agh/miss/Simulation.java
@@ -3,7 +3,7 @@ package pl.edu.agh.miss;
 import net.sourceforge.jswarm_pso.Swarm;
 
 public class Simulation {
-	public static final int NUMBER_OF_DIMENTIONS = 2;
+	public static final int NUMBER_OF_DIMENTIONS = 10;
 	public static final int NUMBER_OF_ITERATIONS = 5000;
 	public static final int NUMBER_OF_SKIPPED_ITERATIONS = 500;
 	public static final int NUMBER_OF_PARTICLES = Swarm.DEFAULT_NUMBER_OF_PARTICLES;
